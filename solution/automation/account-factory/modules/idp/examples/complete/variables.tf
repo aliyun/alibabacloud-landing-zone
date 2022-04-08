2ee6f47028386ef4ccdfc6da02736c1d73cd6d24
longjun.lin <longjun.lin@alibaba-inc.com>
2022-04-08 10:19:08 +0800

    add workflow file

---
+++ solution/automation/account-factory/modules/idp/examples/complete/variables.tf
@@ -0,0 +1,15 @@
+variable "account_id" {
+    type    = string
+    default = ""
+}
+
+variable "sso_provider_name" {
+    type    = string
+    default = ""
+}
+
+variable "encodedsaml_metadata_document" {
+    type    = string
+    default = ""
+}
+
---
+++ solution/automation/account-factory/modules/idp/versions.tf
@@ -0,0 +1,8 @@
+terraform {
+  required_providers {
+    alicloud = {
+      source = "aliyun/alicloud"
+    }
+  }
+  required_version = ">=0.12"
+}
