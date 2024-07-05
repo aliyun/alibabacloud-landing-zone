a5ce1da7023b0f4489eee0fa4da9cffef4d5c882
LandingZone <LandingZone@alibaba-inc.com>
2024-07-05 02:27:19 +0000

    '修改示例代码，调用GetCallerIdentity测试'

--- solution/solution-ack-sts-token/code-example/java/src/main/java/org/example/sdk2_0/CredentialsDefaultSample.java
+++ solution/solution-ack-sts-token/code-example/java/src/main/java/org/example/sdk2_0/CredentialsDefaultSample.java
@@ -2,8 +2,7 @@ package org.example.sdk2_0;
 
 import com.alibaba.fastjson2.JSON;
 import com.aliyun.credentials.Client;
-import com.aliyun.vpc20160428.models.DescribeVpcsRequest;
-import com.aliyun.vpc20160428.models.DescribeVpcsResponse;
+import com.aliyun.sts20150401.models.GetCallerIdentityResponse;
 
 /**
  * 通过Credentials工具初始化，使用默认凭据链
@@ -11,16 +10,14 @@ import com.aliyun.vpc20160428.models.DescribeVpcsResponse;
 public class CredentialsDefaultSample {
     public static void main(String[] args) throws Exception {
         // 初始化凭据客户端
-        Client credentialClient = new Client();
+        Client credentialsClient = new Client();
 
-        // 调用API，以VPC为例
+        // 调用API，以GetCallerIdentity获取当前调用者身份信息为例
         com.aliyun.teaopenapi.models.Config config = new com.aliyun.teaopenapi.models.Config();
-        config.setCredential(credentialClient);
-        config.setEndpoint("vpc.aliyuncs.com");
-        com.aliyun.vpc20160428.Client vpcClient = new com.aliyun.vpc20160428.Client(config);
-
-        DescribeVpcsRequest describeVpcsRequest = new DescribeVpcsRequest().setRegionId("cn-hangzhou");
-        DescribeVpcsResponse describeVpcsResponse = vpcClient.describeVpcs(describeVpcsRequest);
-        System.out.println(JSON.toJSONString(describeVpcsResponse));
+        config.setCredential(credentialsClient);
+        config.setEndpoint("sts.cn-hangzhou.aliyuncs.com");
+        com.aliyun.sts20150401.Client stsClient = new com.aliyun.sts20150401.Client(config);
+        GetCallerIdentityResponse getCallerIdentityResponse = stsClient.getCallerIdentity();
+        System.out.println(JSON.toJSONString(getCallerIdentityResponse));
     }
 }
